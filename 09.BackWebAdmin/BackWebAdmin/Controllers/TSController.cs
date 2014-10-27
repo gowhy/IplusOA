77915375e1e40762a7601eb53702c59cb4c516dd
286379175 <376007434@qq.com>
2014-10-27 21:53:16 +0800

    xx

--- 09.BackWebAdmin/BackWebAdmin/Controllers/TSController.cs
+++ 09.BackWebAdmin/BackWebAdmin/Controllers/TSController.cs
@@ -12,6 +12,7 @@ using System.IO;
 
 namespace BackWebAdmin.Controllers
 {
+    [SecurityModule(Name = "投诉")]
     public class TSController : BaseController
     {
         private static int PageSize = 20;
